5b3f15595deb343addf09478f0e0bf1f42e825a5
Li Xi <xl404@cam.ac.uk>
2019-01-20 08:12:54 +0000

    Added multer and simple route

--- controllers/master.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const fs = require("fs");
-
-// simple routes
-router.use( '/', express.static("./hc/hackcambridge.com"));
-
-module.exports = router;
--- masterServer.js
+++ masterServer.js
@@ -1,8 +1,8 @@
 const express = require( 'express' );
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
-const routes = require("./controllers/master");
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -14,7 +14,12 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
 app.use(bodyParser.json({limit: '50mb'}));
 
-app.use(routes);
+app.post("/upload", upload.single("pkl"), async function (req, res) {
+    // handle upload
+    
+});
+
+
 
 var _port = 8080;
 
